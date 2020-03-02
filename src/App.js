819ce158c4fce5c3e8237dd4020d183a3b9cb16b
TomasTheHuman <fraymark59@gmail.com>
2020-03-02 17:34:12 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+  import React from 'react';
+  import {Table,Modal} from "antd";
+  import "antd/dist/antd.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  class App extends React.Component{
+    state = {
+      mas:[],
+    }
+    async componentDidMount(){
+      await fetch("https://jsonplaceholder.typicode.com/users")
+        .then(Result => Result.json())
+        .then(data =>{this.setState({
+          mas: data
+          })
+        })
+    }
+
+    render() {
+      return (
+      <Table  columns={columns} dataSource={this.state.mas} />
+      );
+    }
+  }
+
+
+  const columns = [
+    {
+      title: 'Имя',
+      dataIndex: 'name',
+    },
+    {
+      title: 'email',
+      dataIndex: 'email',
+    },
+    {
+      title: 'Номер телефона',
+      dataIndex: 'phone',
+    },
+  ];
+
+
+  export default App;
